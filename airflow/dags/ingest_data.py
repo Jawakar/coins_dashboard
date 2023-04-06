19e9930beb1b2be48fa44cbce5f59b7859f65f1d
Jawakar R S <jawakarselvavinayagam@gmail.com>
2023-04-06 18:24:21 +0530

    updated ingest_data.py

--- airflow/dags/ingest_data.py
+++ airflow/dags/ingest_data.py
@@ -16,7 +16,7 @@ default_args = {
     'depends_on_past': False,
     'start_date': datetime(2023, 3, 31),
     'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'retry_delay': timedelta(minutes=5),
 }
 
 def get_data():
@@ -44,7 +44,7 @@ dag = DAG(
     'ingest_data',
     default_args=default_args,
     description='Ingest data to GCP bucket',
-    schedule_interval=timedelta(minutes=1),
+    schedule_interval=timedelta(minutes=5),
 )
 
 ingest_task = PythonOperator(
